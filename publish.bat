4647cff40baba04927b1bee978097965203f36a8
tdrdevos <tristandevos99@gmail.com>
2019-06-03 20:40:13 -0400

    Adding auto publish

---
+++ publish.bat
@@ -0,0 +1,6 @@
+git remote add web-test https://github.com/tdrdevos/web-test.git
+git remote update
+git merge web-test/gh-pages --allow-unrelated-histories
+mv ./web-test/* ./
+git commit -m "Auto update website"
+git push
